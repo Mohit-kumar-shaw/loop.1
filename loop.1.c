9223c1ed5fe090b8ea1e0561b0cb81991136723f
Mohit-kumar-shaw <69642308+Mohit-kumar-shaw@users.noreply.github.com>
2020-10-06 13:09:31 +0530

    increment i,j

--- loop.1.c
+++ loop.1.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main(){
    int i,j;
-   for(i=0;i<6;++i){
-     for(j=0;j<=i;++j){
-       printf("*");
+   for(i=0;i<6;i++){
+     for(j=0;j<=i;j++){
+       printf("*******");
      }
      printf("\n");
    }
